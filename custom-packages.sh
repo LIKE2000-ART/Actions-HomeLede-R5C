626d4106df3cf7ba8004279fcbf4d65af429cab5
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-14 16:17:10 +0800

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -2,3 +2,7 @@
 
 git clone --depth 1 --branch master https://github.com/xiaoqingfengATGH/homeclash.git
 mv homeclash/luci-app-openclash $BUILDROOT/package
+
+
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x 
+rm -rf $BUILDROOT/feeds/packages/lang/golang && mv packages_lang_golang $BUILDROOT/feeds/packages/lang/golang
