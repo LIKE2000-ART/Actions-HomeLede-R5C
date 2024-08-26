57bcae38fe420a89f93ee39bd6b9f6fd6b232758
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-26 23:22:38 +0800

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,11 +1,7 @@
 # Load luci-app-openclash
 
-git clone --depth 1 --branch master https://github.com/xiaoqingfengATGH/homeclash.git
-mv homeclash/luci-app-openclash $BUILDROOT/package
-
-git clone --depth 1 --branch master https://github.com/xiaoqingfengATGH/luci-app-strongswanInDocker
-mv luci-app-strongswanInDocker/luci-app-strongswanInDocker $BUILDROOT/package
-
+git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
+mv OpenClash/luci-app-openclash $BUILDROOT/package
 
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x 
 rm -rf $BUILDROOT/feeds/packages/lang/golang && mv packages_lang_golang $BUILDROOT/feeds/packages/lang/golang
