fddff2c6c340c6399c118b92c3e6445900bfad39
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-26 23:25:50 +0800

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -5,3 +5,6 @@ mv OpenClash/luci-app-openclash $BUILDROOT/package
 
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x 
 rm -rf $BUILDROOT/feeds/packages/lang/golang && mv packages_lang_golang $BUILDROOT/feeds/packages/lang/golang
+
+git clone --depth 1 --branch main https://github.com/sirpdboy/luci-app-ddns-go.git
+mv luci-app-ddns-go $BUILDROOT/package
