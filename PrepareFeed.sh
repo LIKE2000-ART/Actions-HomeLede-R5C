9a50d43789a222d98e650fdcc2b06f40dbc094f1
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-27 19:33:14 +0800

    Update PrepareFeed.sh

--- PrepareFeed.sh
+++ PrepareFeed.sh
@@ -1,7 +1,7 @@
 
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+rm -rf $BUILDROOT/feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 
-rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+rm -rf $BUILDROOT/feeds/luci/applications/luci-app-mosdns && rm -rf $BUILDROOT/feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf $BUILDROOT/feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang $BUILDROOT/feeds/packages/lang/golang
