6f1feba1b814915b44626333acd05240a506b804
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-28 14:52:15 +0800

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,4 +1,4 @@
 # Load luci-app-openclash
 
 git clone --depth 1 --branch master https://github.com/sirpdboy/luci-app-partexp
-mv OpenClash/luci-app-partexp $BUILDROOT/package
+mv luci-app-partexp $BUILDROOT/package
