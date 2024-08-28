6de0f0f98592a5c611f8fda0fd074e2bf0df99b5
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-28 16:00:22 +0800

    Update custom-feeds.sh

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -1,6 +1,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' $BUILDROOT/feeds.conf.default
-rm -rf  $BUILDROOT/feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILDROOT/feeds.conf.default
