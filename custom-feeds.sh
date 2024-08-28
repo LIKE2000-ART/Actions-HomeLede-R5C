5d4005fe691eb9a881e85683f6cf12c265227237
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-28 16:44:25 +0800

    Update custom-feeds.sh

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -4,5 +4,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILDROOT/feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> $BUILDROOT/feeds.conf.default
-sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' >> $BUILDROOT/feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' $BUILDROOT/feeds.conf.default
 
