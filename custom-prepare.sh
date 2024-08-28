c0e60629cc11ae0e712ea1cc5340f3d7b04a2747
LIKE2000-ART <55705638+LIKE2000-ART@users.noreply.github.com>
2024-08-28 16:37:07 +0800

    Update custom-prepare.sh

--- custom-prepare.sh
+++ custom-prepare.sh
@@ -1,7 +1,6 @@
+rm -rf $BUILDROOT/feeds/luci/applications/luci-app-mosdns && rm -rf $BUILDROOT/feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
 
 rm -rf $BUILDROOT/feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
-
-rm -rf $BUILDROOT/feeds/luci/applications/luci-app-mosdns && rm -rf $BUILDROOT/feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
 
 rm -rf $BUILDROOT/feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang $BUILDROOT/feeds/packages/lang/golang
